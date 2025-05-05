5733fbab23bb62c2861086b46ef154d85750fbc2
hohland <bartezzwajs@gmail.com>
2025-05-05 22:14:38 +0200

    Making an exception in case if there was an issue with connection to DB and make user inputs in get-available-spaces endpoint, parameters mandatory

--- backend/endpoint.py
+++ backend/endpoint.py
@@ -1,7 +1,8 @@
 import os
 
 import mysql.connector
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from datetime import datetime
 
 def queryDB(db, query):
     cursor = db.cursor()
@@ -10,66 +11,85 @@ def queryDB(db, query):
 
     return result
 
-db = mysql.connector.connect(
-    host="mysql",
-    port=3306,
-    user=os.environ.get("DB_USER"),
-    password=os.environ.get("DB_PASSWORD"),
-    database="parking-app",
-)
+def connectToDB(host, port, user, password, database):
+    db = mysql.connector.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=database,
+    )
+    
+    return db
 
+host="mysql"
+port=3306
+user=os.environ.get("DB_USER")
+password=os.environ.get("DB_PASSWORD")
+database="parking-app"
 app = FastAPI()
 
 
 @app.get("/parking-spaces")
 def parkingspaces():
-    result = queryDB(db, "SELECT * FROM `parking-spaces` ORDER BY `ID`")
+    try:
+        db=connectToDB(host, port, user, password, database)
+        result=queryDB(db, "SELECT * FROM `parking-spaces` ORDER BY `ID`")
 
-    json_response = [
-        {
-            "ID": record[0], 
-            "parking-space": record[1]
-        } 
-        for record in result
-    ]
+        json_response=[
+            {
+                "ID": record[0], 
+                "parking-space": record[1]
+            } 
+            for record in result
+        ]
 
-    return json_response
-
+        return json_response
+    except:
+        return "There was an issue with connection to database. Please try again later."
 
 @app.get("/reservations")
 def reservations():
-    result = queryDB(db, "SELECT * FROM `reservations` ORDER BY `ID`")
+    try:
+        db=connectToDB(host, port, user, password, database)
+        result=queryDB(db, "SELECT * FROM `reservations` ORDER BY `ID`")
 
-    json_response = [
-        {
-            "ID": reservation[0],
-            "start": reservation[1],
-            "end": reservation[2],
-            "parking-space": reservation[3],
-            "confirmed-reservation": reservation[4],
-        }
-        for reservation in result
-    ]
+        json_response=[
+            {
+                "ID": reservation[0],
+                "start": reservation[1],
+                "end": reservation[2],
+                "parking-space": reservation[3],
+                "confirmed-reservation": reservation[4],
+            }
+            for reservation in result
+        ]
 
-    return json_response
+        return json_response
+    except:
+        return "There was an issue with connection to database. Please try again later."
 
 
 @app.get("/get-available-spaces")
-def availablespaces(start_time, end_time):
-    result = queryDB(db, 
-        f"SELECT `parking-space` fROM `parking-app`.`parking-spaces` \
-        WHERE `parking-space` NOT IN ( \
-        SELECT `parking-space` fROM `parking-app`.`reservations` \
-        WHERE NOT (end <= \"{start_time}\" OR start >= \"{end_time}\") \
-        ) \
-        ORDER BY `ID`"
-    )
+def availablespaces(start_time: datetime = Query(description="Start time of reservation (YYYY-MM-DD HH:MM:SS format)"), end_time: datetime = Query(description="End time of your reservation (YYYY-MM-DD HH:MM:SS format)")):
+    try:
+        db=connectToDB(host, port, user, password, database)
+        result=queryDB(db, 
+            f"SELECT `parking-space` FROM `parking-app`.`parking-spaces` \
+            WHERE `parking-space` NOT IN ( \
+            SELECT `parking-space` FROM `parking-app`.`reservations` \
+            WHERE NOT (end <= \"{start_time}\" OR start >= \"{end_time}\") \
+            ) \
+            ORDER BY `ID`"
+        )
 
-    json_response = [
-        {
-            "parking-space": record[0]
-        }
-        for record in result
-    ]
+        json_response=[
+            {
+                "parking-space": record[0]
+            }
+            for record in result
+        ]
 
-    return json_response
+        return json_response
+    except:
+        return "There was an issue with connection to database. Please try again later."
