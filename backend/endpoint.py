0aa26c2d82fe8a2a7441fdc1dd6916c3fd71224f
hohland <bartezzwajs@gmail.com>
2025-05-07 00:32:01 +0200

    Added new endpoint for making reservations

--- backend/endpoint.py
+++ backend/endpoint.py
@@ -11,6 +11,11 @@ def queryDB(db, query):
 
     return result
 
+def insertIntoDB(db, query, params):
+    cursor = db.cursor()
+    cursor.execute(query, params)
+    db.commit()
+
 def connectToDB(host, port, user, password, database):
     db = mysql.connector.connect(
         host=host,
@@ -71,14 +76,14 @@ def reservations():
 
 
 @app.get("/get-available-spaces")
-def availablespaces(start_time: datetime = Query(description="Start time of reservation (YYYY-MM-DD HH:MM:SS format)"), end_time: datetime = Query(description="End time of your reservation (YYYY-MM-DD HH:MM:SS format)")):
+def availablespaces(startTime: datetime = Query(description="Start time of reservation (YYYY-MM-DD HH:MM:SS format)"), endTime: datetime = Query(description="End time of your reservation (YYYY-MM-DD HH:MM:SS format)")):
     try:
         db=connectToDB(host, port, user, password, database)
         result=queryDB(db, 
             f"SELECT `parking-space` FROM `parking-app`.`parking-spaces` \
             WHERE `parking-space` NOT IN ( \
             SELECT `parking-space` FROM `parking-app`.`reservations` \
-            WHERE NOT (end <= \"{start_time}\" OR start >= \"{end_time}\") \
+            WHERE NOT (end <= \"{startTime}\" OR start >= \"{endTime}\") \
             ) \
             ORDER BY `ID`"
         )
@@ -93,3 +98,37 @@ def availablespaces(start_time: datetime = Query(description="Start time of rese
         return json_response
     except:
         return "There was an issue with connection to database. Please try again later."
+    
+@app.post("/make-reservation")
+def makereservation(parkingSpot: str = Query("Parking spot which you would like to reserve"), startTime: datetime = Query(description="Start time of reservation (YYYY-MM-DD HH:MM:SS format)"), endTime: datetime = Query(description="End time of your reservation (YYYY-MM-DD HH:MM:SS format)")):
+    # try:
+        db=connectToDB(host, port, user, password, database)
+        checkParkingSpace=queryDB(db,
+            f"SELECT `parking-space` FROM `parking-app`.`parking-spaces` \
+            WHERE `parking-space` NOT IN ( \
+            SELECT `parking-space` FROM `parking-app`.`reservations` \
+            WHERE NOT (end <= \"{startTime}\" OR start >= \"{endTime}\") \
+            ) \
+            ORDER BY `ID`"
+        )
+
+        for parkingSpace in checkParkingSpace:
+            if parkingSpot == str(parkingSpace[0]):
+                availableSpace = True
+                break
+            else:
+                availableSpace = False
+
+        if availableSpace:
+            insertIntoDB(db,
+                    f"INSERT INTO `parking-app`.`reservations` \
+                    (`start`, `end`, `parking-space`, `confirmed-reservation`) \
+                    VALUES(%s, %s, %s, %s)",
+                    (startTime, endTime, parkingSpot, 0)          
+            )
+            return f"Confirmed reservation for parking space {parkingSpot}. Start time: {startTime}, end time: {endTime}."
+        else:
+            return "The parking spot that you have selected is not available during specified time."
+
+    # except:
+    #     return "There was an issue with connection to database. Please try again later."
