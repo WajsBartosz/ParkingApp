72e1226b314a3c74e90c9bb6a4e9c868dba10fca
hohland <bartezzwajs@gmail.com>
2025-05-17 15:25:43 +0200

    removed pushContainers.sh (replaced by pushWebApp.sh)

--- deployment/pushContainers.sh
+++
@@ -1,4 +0,0 @@
-docker build -t parkingappcontainerregistry.azurecr.io/parking-app-backend:latest ../backend
-docker push parkingappcontainerregistry.azurecr.io/parking-app-backend:latest
-docker build -t parkingappcontainerregistry.azurecr.io/parking-app-frontend:latest ../frontend
-docker push parkingappcontainerregistry.azurecr.io/parking-app-frontend:latest
