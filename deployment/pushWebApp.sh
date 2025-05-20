61d977cdb37da4e1f78cfa147d9774ee71d6abaa
hohland <bartezzwajs@gmail.com>
2025-05-20 18:35:45 +0200

    added a functionality to create webapp for frontend and creation of azure function resource

--- deployment/pushWebApp.sh
+++ deployment/pushWebApp.sh
@@ -10,3 +10,4 @@ docker push parkingappcontainerregistry.azurecr.io/parking-app-frontend:latest
 
 az webapp create -g "parking-app-rg" --name "parking-app-backend" -p "parking-app-web-app-service-plan" --deployment-container-image-name "parkingappcontainerregistry.azurecr.io/parking-app-backend:latest"
 az webapp config appsettings set --name "parking-app-backend" --resource-group "parking-app-rg" --settings DB_ADDRESS=$DB_ADDRESS DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD DB_DATABASE=$DB_DATABASE
+az webapp create -g "parking-app-rg" --name "parking-app-frontend" -p "parking-app-web-app-service-plan" --deployment-container-image-name "parkingappcontainerregistry.azurecr.io/parking-app-frontend:latest"
