918a3d6a0f98b6d34c67366b564017087984b827
BFKazmierczak <bfkazmierczak@gmail.com>
2025-05-21 11:18:18 +0200

    feat(frontend): parking map work in progress

--- frontend/src/pages/reservation/components/ParkingMap.tsx
+++ frontend/src/pages/reservation/components/ParkingMap.tsx
@@ -6,39 +6,77 @@ import {
 } from "../../../features/reservation/queries";
 import useReservationContext from "../providers/ReservationProvider/hooks";
 import styles from "./ParkingMap.module.css";
-import ParkingSpace from "./ParkingSpace";
 import ReservationSidebar from "./ReservationSidebar";
+import { ParkingSpace } from "../../../features/reservation/types";
 
 type ParkingSpaceMeta = {
   x: number;
   y: number;
-  width: number;
-  height: number;
-  reversed?: boolean;
 };
+
+type ParkingSpaceMap = Map<string, ParkingSpaceMeta>;
+
+type ParkingSpaceRow = {
+  x: number;
+  y: number;
+  spacesMap: ParkingSpaceMap;
+};
+
+const pillarSize = 700;
+const padding = 200;
+const width = 1000;
+const height = 2000;
 
 const pillars = [
   {
-    x: 170,
-    y: 10,
-    width: 55,
-    height: 55,
+    x: 0 + padding,
+    y: 0 + padding,
+  },
+  {
+    x: 0 + padding,
+    y: 10000 - pillarSize - padding,
+  },
+  {
+    x: 10000 - padding - pillarSize,
+    y: 0 + padding,
+  },
+  {
+    x: 10000 - padding - pillarSize,
+    y: 10000 - padding - pillarSize,
+  },
+  {
+    x: 0 + padding,
+    y: 10000 - pillarSize - padding,
+  },
+  {
+    x: 1600 + 3 * (width + padding),
+    y: 2200,
   },
 
   {
-    x: 110,
-    y: 220,
-    width: 55,
-    height: 55,
+    x: 1600 + 3 * (width + padding),
+    y: 2200 + height - pillarSize,
   },
-];
+] as const;
 
-const parkingSpacesMap = new Map<string, ParkingSpaceMeta>([
-  ["A1", { x: 70, y: 10, width: 40, height: 80 }],
-  ["A2", { x: 120, y: 10, width: 40, height: 80 }],
-  ["A3", { x: 60, y: 195, width: 40, height: 80 }],
-  ["A4", { x: 10, y: 195, width: 40, height: 80 }],
-]);
+const parkingSpaceRows = [
+  {
+    x: 1600,
+    y: 2200,
+    spaces: ["A1", "A2", "A3"],
+  },
+  {
+    x: 8000 - padding,
+    y: 4200,
+    spaces: ["A4", "A5", "A6"],
+    direction: "horizontal",
+  },
+  {
+    x: 1600,
+    y: 10000 - height - padding,
+    spaces: ["A7", "A8", "A9", "A10", "A11", "A12"],
+  },
+] as const;
 
 interface Props {}
 
@@ -57,6 +95,14 @@ function ParkingMap({}: Props) {
       startTime: filters.date && format(filters.date, "yyyy-MM-dd HH:MM:SS"),
       endTime: filters.date && format(filters.date, "yyyy-MM-dd HH:MM:SS"),
     });
+
+  const allSpacesMap = useMemo<Map<string, ParkingSpace>>(() => {
+    if (!allSpacesData) return new Map([]);
+
+    return new Map<string, ParkingSpace>(
+      allSpacesData.map((space) => [space["parking-space"], space]),
+    );
+  }, [allSpacesData]);
 
   useEffect(() => {
     console.log({ allSpacesData });
@@ -87,108 +133,108 @@ function ParkingMap({}: Props) {
         <span className="info-text">Wybierz miejsce parkingowe</span>
       </div>
 
-      <svg
-        ref={parkingRef}
-        viewBox="0 0 800 400"
-        preserveAspectRatio="xMidYMid meet"
+      <div
+        style={{
+          width: "100%",
+          height: 500,
+        }}
       >
-        <rect
-          width="100%"
-          height="100%"
-          stroke="black"
-          fill="#e5e5e5"
-          rx={10}
-        />
+        <svg
+          ref={parkingRef}
+          className={styles["svg-container"]}
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          viewBox="0 0 10000 10000"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <rect
+            id="parking-border"
+            width="100%"
+            height="100%"
+            stroke="black"
+            fill="#e5e5e5"
+            rx={10}
+          />
 
-        <rect
-          id="gate"
-          stroke="black"
-          rx={8}
-          x={0}
-          y={100}
-          width={20}
-          height={200}
-        />
-
-        {allSpacesData?.map((space, index) => {
-          const mapEntry = parkingSpacesMap.get(space["parking-space"]);
-
-          console.log("Map entry:", mapEntry);
-
-          if (!mapEntry) return null;
-
-          return (
-            <>
-              <rect
-                key={space["parking-space"]}
-                style={{ cursor: "pointer" }}
-                fill="white"
-                stroke="black"
-                x={mapEntry.x}
-                y={mapEntry.y}
-                width={mapEntry.width}
-                height={mapEntry.height}
-                // fill={selected === space.id ? "green" : "gray"}
-                onClick={() => setSelectedSpace(space)}
-              />
-
-              <text
-                fill="black"
-                fontSize="16"
-                textAnchor="middle"
-                alignmentBaseline="middle"
-                x={mapEntry.x + mapEntry.width / 2}
-                y={mapEntry.y + mapEntry.height / 2}
-              >
-                {space["parking-space"]}
-              </text>
-            </>
-          );
-        })}
-
-        {pillars.map((pillar) => {
-          return (
+          <svg>
             <rect
-              stroke="black"
-              rx={5}
-              x={pillar.x}
-              y={pillar.y}
-              width={pillar.width}
-              height={pillar.height}
+              id="gate"
+              fill="black"
+              rx={8}
+              x={0}
+              y={5000 - 1200}
+              width={200}
+              height={4000}
             />
-          );
-        })}
 
-        {/* <svg */}
-        {/*   xmlns="http://www.w3.org/2000/svg" */}
-        {/*   x={10} */}
-        {/*   y={10} */}
-        {/*   viewBox="0 0 24 24" */}
-        {/*   width="4em" */}
-        {/*   height="4em" */}
-        {/* > */}
-        {/*   <path */}
-        {/*     fill="none" */}
-        {/*     stroke="currentColor" */}
-        {/*     strokeLinecap="round" */}
-        {/*     strokeLinejoin="round" */}
-        {/*     strokeWidth="2" */}
-        {/*     d="M5 12h14M5 12l4-4m-4 4l4 4" */}
-        {/*   ></path> */}
-        {/* </svg> */}
-      </svg>
+            {pillars.map((pillar) => (
+              <rect x={pillar.x} y={pillar.y} width={700} height={700} />
+            ))}
 
-      {/* {allSpacesData?.map((space, index) => ( */}
-      {/*   <ParkingSpace */}
-      {/*     key={space["parking-space"]} */}
-      {/*     space={space} */}
-      {/*     disabled={disabledSpaces.has(space["parking-space"])} */}
-      {/*     onClick={() => setSelectedSpace(space)} */}
-      {/*   /> */}
-      {/* ))} */}
+            {parkingSpaceRows.map((row, index) => (
+              <svg
+                style={{
+                  width: "100%",
+                  height: "auto",
+                }}
+                x={row.x}
+                y={row.y}
+              >
+                {row.spaces.map((spaceKey, spaceIndex) => {
+                  const spaceData = allSpacesMap.get(spaceKey);
 
-      {/* {isFetchingAvailableSpaces && <p>Pobieranie wolnych miejsc</p>} */}
-      {/* {data?.map((parkingSpace) => <ParkingSpace space={parkingSpace} />)} */}
+                  if (!spaceData) return null;
+
+                  console.log("Row:", row);
+                  let x = (width + padding) * spaceIndex;
+                  let y = 0;
+
+                  let textX = x + width / 2;
+                  let textY = y + height / 2;
+
+                  if (row.direction === "horizontal") {
+                    x = 0;
+                    y = (width + padding) * spaceIndex;
+
+                    textX = x + height / 2;
+                    textY = y + width / 2;
+                  }
+
+                  return (
+                    <>
+                      <rect
+                        key={spaceData["parking-space"]}
+                        style={{ cursor: "pointer" }}
+                        fill="white"
+                        stroke="black"
+                        x={x}
+                        y={y}
+                        width={row.direction === "horizontal" ? height : width}
+                        height={row.direction == "horizontal" ? width : height}
+                        // fill={selected === space.id ? "green" : "gray"}
+                        onClick={() => setSelectedSpace(spaceData)}
+                      />
+
+                      <text
+                        fill="black"
+                        fontSize={32 * 10}
+                        textAnchor="middle"
+                        alignmentBaseline="middle"
+                        x={x + width / 2}
+                        y={y + height / 2}
+                      >
+                        {spaceData["parking-space"]}
+                      </text>
+                    </>
+                  );
+                })}
+              </svg>
+            ))}
+          </svg>
+        </svg>
+      </div>
 
       <ReservationSidebar
         visible={selectedSpace !== undefined}
